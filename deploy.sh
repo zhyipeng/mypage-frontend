4d55181c8b5ae70e0f1faa5264e969aa3272f9e2
zhyipeng <zhangyipeng@hogecloud.com>
2020-03-15 16:29:27 +0800

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:zhyipeng/zhyipeng.github.io.git master
+git push -f git@github.com:zhyipeng/zhyipeng.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zhyipeng/mypage-frontend.git master:gh-pages
+# git push -f git@github.com:zhyipeng/mypage-frontend.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,1 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/mypage-frontend/' : '/'
-}
+module.exports = {}
